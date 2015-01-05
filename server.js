ac46378455118bd8b716a91d6308516b7e0cc638
Jake Pruitt <jrpruit1@asu.edu>
2015-01-05 17:05:25 -0600

    Bootstrapped application

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+var Hapi = require('hapi');
+
+var server = new Hapi.Server();
+server.connection({ port: 4000 });
+
+server.route({
+  method: 'GET',
+  path: '/{param*}',
+  handler: {
+    directory: {
+      path: 'client'
+    }
+  }
+});
+
+server.start(function () {
+  console.log('Server running at:', server.info.uri);
+});
