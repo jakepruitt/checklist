afb60f3e5342bea5e49abafc0c3f62fbb6f2f910
Jake Pruitt <jrpruit1@asu.edu>
2015-01-09 16:06:37 -0600

    Setting up multiple servers

--- server.js
+++ server.js
@@ -4,16 +4,29 @@ var server = new Hapi.Server();
 var web = server.connection({ port: 4000 });
 var api = server.connection({ port: 4040 });
 
-server.route({
+web.route({
   method: 'GET',
   path: '/{param*}',
   handler: {
     directory: {
       path: 'client'
     }
+  },
+  config: {
+    description: 'Web app static content',
+    notes: 'Set up to respond to web addressing',
+    tags: ['app', 'checklist', 'spa']
+  }
+});
+
+api.route({
+  method: 'GET',
+  path: '/',
+  handler: function(request, reply) {
+    reply('There is always money in the banana boat');
   }
 });
 
 server.start(function () {
-  console.log('Server running at:', server.info.uri);
+  console.log('Server running at:', server.connections);
 });
