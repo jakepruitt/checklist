ce743c524fb453507309f3a48d08d62265189a3d
Jake Pruitt <jrpruit1@asu.edu>
2015-01-11 16:54:33 -0600

    Edited server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 var Hapi = require('hapi');
+var seneca = require('seneca')();
+var app = require('express')();
 
 var server = new Hapi.Server();
 var web = server.connection({ port: 4000 });
@@ -27,6 +29,22 @@ api.route({
   }
 });
 
+seneca.add({role: 'test', cmd: 'test'}, function(args, done) {
+  console.log('Recieved a request');
+  done(null, {worked: true});
+});
+
+seneca.act('role:web', {use: {
+  prefix: '/api/1.0',
+  pin: {role:'test', cmd:'*'},
+  map: {
+    test: true
+  }
+}});
+
+app.use(seneca.export('web'));
+app.listen(3000);
+
 server.start(function () {
-  console.log('Server running at:', server.connections);
+  console.log('Server running at: %d and %d', server.connections[0].info.port, server.connections[1].info.port);
 });
