550b3e6e1443d62be5787c7242d15aad495caac2
Jake Pruitt <jrpruit1@asu.edu>
2015-01-14 21:09:00 -0700

    Trying to do it with a hapi plugin

--- hapi-seneca.js
+++ hapi-seneca.js
@@ -6,13 +6,23 @@ var hapiSeneca = {
     debugger;
     seneca.act('role:web, cmd:routes', function(err, routes) {
       console.log(routes);
-      var testRoute = routes[0];
-      server.route({
-        method: testRoute.method,
-        path: testRoute.url,
-        handler: function(request, reply) {
-          reply('yeah, buddy');
-        }
+      seneca.act('role:web, cmd:list', function(err, services) {
+        var testRoute = routes.filter(function(route) { 
+          return route.url === '/api/1.0/test' 
+        }).shift(); 
+        console.log(testRoute);
+        var testService = services.filter(function(service) {
+          return service.serviceid$ === testRoute.service.serviceid 
+        }).shift();
+        console.log(testService);
+        server.route({
+          method: testRoute.method,
+          path: testRoute.url,
+          handler: function(req, reply) {
+            console.log('Handler called!');
+            testService(req,reply('Hello world!'),next);
+          }
+        });
       });
     });
     next();
