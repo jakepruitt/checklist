40d7cd7268487157d8a0b5d2805da44d5547abb0
Jake Pruitt <jrpruit1@asu.edu>
2015-01-20 11:30:54 -0700

    Changed hapi-seneca middleware

--- hapi-seneca.js
+++ hapi-seneca.js
@@ -1,25 +1,26 @@
 var hapiSeneca = {
   register: function (server, options, next) {
-    var seneca = options.seneca;
+    var seneca = options.seneca,
+        status = 200,
+        headers = {};
+
+    server.decorate('reply', 'writeHead', function(resStatus, resHeaders) {
+      status = resStatus;
+      headers = resHeaders;
+    });
+
+    server.decorate('reply', 'end', function(objstr) {
+      var reply = this,
+          res = reply(objstr);
+      res.code(status);
+      for(header in headers) {
+        res.header(header, headers[header]);
+      }
+    });
     
     server.ext('onRequest', function(request, reply) {
       var req = request.raw.req;
-      var status = 200;
-      var headers = {};
-      var res = {
-        writeHead: function(resStatus, resHeaders) { 
-          status = resStatus;
-          headers = resHeaders;
-        },
-        end: function(objstr) { 
-          var res = reply(objstr); 
-          res.code(status);
-          for (header in headers) {
-            res.header(header, headers[header]);
-          }
-        }
-      };
-      seneca.export('web')(req, res, function(err) {
+      seneca.export('web')(req, reply, function(err) {
         if (err) { return reply(err); }
         reply.continue();
       });
